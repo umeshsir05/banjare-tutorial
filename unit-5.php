d1a1345040fe6d2fa722c07120fe938f1f5ccbbc
umeshsir05 <ukbanjare1989@gmail.com>
2025-09-30 21:19:21 +0530

    Add files via upload
    Uploaded 10 files.

---
+++ unit-5.php
@@ -0,0 +1,225 @@
+<?php include('db.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body style="font:'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', 'DejaVu Sans', 'Verdana, sans-serif'; font-size:16px">
+    <header>
+        <div class="logo">
+            <h1>Umesh Banjare</h1>
+        </div>
+        <nav class="nav-menu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="notes.php">Notes</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="admin.php">Admin</a></li>
+            </ul>
+        </nav>
+        <div class="menu-toggle">
+            <span>&#9776;</span>
+        </div>
+    </header>
+<main>
+        <h2>Study Notes for IT</h2>
+        <ul id="textheight">
+           <li> <h3 align="center">Unit -5 </h3>
+             <h3 align="center"><u><strong>Digital Presentation <a href="#">(Download notes)</a></strong><a href="#"></a></u></h3>
+           </li>
+		</ul>
+        <pre>
+<strong>A. Multiple Choice Questions</strong>
+1. Which of the following option is not available on Presentation Wizard?
+<u><strong>(a) Empty presentation</strong></u> 		(b) Form template
+(c) Open new presentation      (d) Open existing presentation
+
+2. Which of the following is not a part of main Impress window?
+(a) Slides pane				 (b) Workspace
+(c) <u><strong>Work pane</strong></u> 				(d) Task pane
+
+3. Which of the following is not a section of tasks pane?
+(a) Master pages			 (b) Layouts
+(c) <u><strong>Custom View</strong></u> 			(d) Custom animation
+
+4. Which view button listed below is not one of those available in the workspace?
+(a) Normal view 			(b) Outline view
+(c) <u><strong>Thumbnail view</strong></u>			(d) Notes
+
+5. Which view is generally used for creating, formatting and designing slides?
+(a) <u><strong>Normal view</strong></u> 			(b) Outline view
+(c) Notes 					(d) Slide Sorter view
+
+6. The slide show can be exited at any time during the show by pressing which of the following keys?
+(a) Space bar 				(b) End key
+(c) Break key				(d) <u><strong>Esc key</strong></u>
+
+7. Which of the following features is used to create a new slide show with the current slides but presented in a different order?
+(a) Rehearsal 				(b)<u><strong> Custom Slide show</strong></u>
+(c) Slide Show Setup 		(d) Slide Show View
+
+8. Which of the following feature is used to progress the slide show automatically while speaking on the topic?
+(a) Custom Animation 		(b) Rehearse Timing
+(c) <u><strong>Slide Transition</strong></u> 			(d) Either (a) or (b)
+
+<strong>B. Fill in the blanks</strong>.
+
+1. <u><strong>Master slide</strong></u> is used to maintain consistency in design and colour in the presentation.
+2. <u><strong>Slide sorter</strong></u> view is used to view all the slides simultaneously.
+3.<u><strong> File menu </strong></u>is used to perform basic operations on the presentation
+4. Master Page is used to modify the<u><strong> base architecture</strong></u> of the slide.
+5. To create a new blank presentation, use the key combination<strong><u> Ctrl+N</u></strong>.
+6. In every presentation, first slide should be <u><strong>Title slide</strong></u>.
+7. To save a presentation, we can use key combination <u><strong>Ctrl+S</strong></u> .
+8. In LibreOffice Impress, by default the presentation is saved with <u><strong>.odp</strong></u> extension.
+9. The keyboard shortcut key for slide show is<u><strong> F5 </strong></u>.
+10. The short cut key to close the LibreOffice impress is <u><strong>Ctrl+W</strong></u> .
+11. The short cut key to insert a new slide is <strong><u>Ctrl+M</u></strong>.
+12. The <u><strong>Normal view</strong></u>  is used to apply animation on the content of slide
+13. A paper copy of presentation given to the audience is known as <strong><u>Handouts</u></strong>
+14. To play a sound during transitions, select a sound from the  <strong><u>Sound</u></strong>  list.
+15. To play the sound repeatedly, the <u><strong>loop until next sound</strong></u> is used.
+
+<strong>C. State whether the following statements are True or False</strong>.
+
+1. The order of the slides cannot be changed in slides pane. <strong>[True]</strong>
+2. Slide design or layout can be changed for multiple slides simultaneously. <strong>[True]</strong>
+3. Every slide in a presentation has exactly one slide master. <strong>[True]</strong>
+4. Animations once applied can be changed but cannot be removed. <strong>[False]</strong>
+5. Slide names are included in outline view. <strong>[True]</strong>
+6. The notes added to slides can be seen during the presentation. <strong>[True]</strong>
+7. A presentation can have multiple slide masters. <strong>[False]</strong>
+8. A user can create his/her own slide master. <strong>[True]</strong>
+9. Once a pre-defined slide master is selected, the background of slide cannot be changed. <strong>[False]</strong>
+10. The text added to the header is displayed on the first slide only.<strong> [False]</strong>
+11. The text added to the footer is displayed on the last slide only. <strong>[False]</strong>
+12. User can create his/her own template and use it in the Presentation Wizard.<strong> [True]</strong>
+13. The Notes View is used for the audience. <strong>[False]</strong>
+14. It is not possible to insert audio or video clips in the presentation.<strong>[False]</strong>
+15. Header and footer can be inserted in the presentation. <strong>[True]</strong>
+
+<strong>D. Short answer questions (50 words)</strong>
+
+<strong>Q1. List the possible multimedia contents that are included while creating a presentation.</strong>
+<strong>Ans.</strong> Possible multimedia contents that are included while creating a presentation are:
+1. Audio
+2. Video
+3. Image
+4. Animation
+
+<strong>Q.2. List the important points to be considered while making
+an effective presentation.</strong>
+<strong>Ans.</strong> Important points to be considered while making an effective presentation are :
+(a) On one page or slide try to include 5 to 8 lines.
+(b) In presentation keep the font size appropriate so that the audience can easily read the contents.
+(c) The grammar and language should be correct in your presentation.
+(d) Try to avoid inserting more than two graphics (images, drawings, tables or charts) in any slide.
+(e) Do not include more than one animation or video in one slide.
+(f) Pay attention to target group to meet the requirements of the target audience.
+
+<strong>Q3. What are the advantages of using a presentation?</strong>
+<strong>Ans.</strong> Advantages of using a presentation are:
+1. The concepts that are difficult to explain by teacher, can be easily presented to the audience in a simple way.
+2. Machine parts and operation of various machines can be easily shown
+
+<strong>Q4. What objects can be inserted to slides in Impress?
+Ans.</strong> Various objects can be inserted to slides in Impress are:
+1. Tables: For organizing data.
+2. Shapes: Various graphical elements like rectangles, circles, etc.
+3. Textbox: For adding text.
+4. Charts: To represent data visually.
+
+
+<strong>Q5. What are the steps to add picture or object to the slide?
+Ans.</strong>To insert an image in your document, position the cursor where you want to insert the file, select Insert Image.
+OR
+Click on the insert image icon, located below the Formatting Toolbar. Select the image file and click on Open button or just double click on the image file. The image will get inserted in the file.
+
+<strong>Q6. How can text be added to header or footer on the sliders?
+Ans.</strong> Steps to add header and footer on slides are:
+1. Click Insert -> Header and Footer.
+2. Header and Footer dialog box appears.
+3. Select Footer check box and write text in Footer text box.
+4. Click Apply to All button to apply footer on all slides.
+<strong>
+Q7. Describe the use of fields available in header and footer.
+Ans.</strong> Fields available in header and footer are:
+1. Header: A header is a text/section which appear at the top of the slide.
+2. Footer: A footer is a text/section which appear at the bottom of the slide.
+3. Date & Time: This option helps to add Date and Time in header section of slide.
+4. Page Number: This option helps to add page number on slide.
+
+<strong>Q8. Write the steps to create a template.
+Ans.</strong> Steps to create a template are:
+1. Create a slide which you want to save it as a template.
+2. Click on File --> Templates -> Save As Template.
+3. Save As Template dialog box appear.
+4. Write the name of the template and select the Template category where you want to save.
+5. Click on Save button.
+<strong>
+Q9. Write down the steps to add slide transition in your presentation.
+Ans.</strong> Steps to add slide transition in your presentation are:
+1. In the Sidebar, select the Slide Transition icon.
+2. Select the slides to apply the transition. If you want to apply the transition to all the slides, do not select any slides.
+3. Select a sound from the Sound list.
+4. Select how to advance to the next slide: manually (By mouse click) or automatically.
+5. To apply transition to all slides, click Apply to All Slides.
+6. Click Slide show to view the applied transition.
+
+<strong>Q10. How will you add the slide number at the bottom of each slide?
+Ans. -</strong> Steps to add the slide number at the bottom of each slide are :
+1. Click Insert -> Header and Footer.
+2. Header and Footer dialog box appears.
+3. In Slide tab, Select Slide Number check box.
+4. Click Apply to All button.
+
+<strong>Q11. How will you insert a company's logo (picture) in first slide of your presentation?
+Ans.</strong>  Steps to insert a company's logo (picture) in first slide of your presentation are :
+1. Open your presentation and go to the first slide.
+2. Click on Insert ->Image.
+3. Select your company logo/image and click on Open.
+4. Move the company logo at desired position.
+<strong>
+Q12. How will you add the name of the company on the top of the each slide?
+Ans.-</strong>  Steps to add the name of the company on the top of the each slide are :
+1. Click Insert -> Header and Footer
+2. Header and Footer dialog box appears.
+3. In Notes and Handouts tab, Select Header.
+4. Write name of the company in Header text box.
+5. Click Apply to All button.
+<strong>
+Q13. Write down the steps to create a table in a presentation.
+Ans.-</strong> Steps to create a table in a presentation are:
+1. Click on slide where you want to insert table.
+2. Select Insert -> Table
+3. Specify the number of rows and columns
+4. Click OK
+
+<strong>Q14. Write down the steps to insert a chart in slide.
+Ans.-</strong> Steps to insert a chart in slide are :
+1. Click on slide where you want to insert chart.
+2. Select Insert -> Chart
+3. Chart based on default value will be inserted in slide.
+4. Right click on chart and change the Chart Data Table, Chart type etc.
+<strong>
+Q15. What are the five views of presentation?
+Ans.-</strong> The five views of presentation are:
+1. Normal View: Default view for creating and editing slides.
+2. Outline View: Displays slide content in outline form.
+3. Notes View: Shows slides with speaker notes.
+4. Slide Sorter View: Arranges slides in a grid for easy organization.
+5. Handout View: Designs print layouts for audience handouts.
+        </pre>
+
+</main>
+   <footer style="background-color: #333; color: #fff; text-align: center; padding: 10px 0; width: 100%;">
+    <p>&copy; <b>2025 Umesh Banjare. All rights reserved. Developed by Umesh Banjare.</b></p>
+</footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
